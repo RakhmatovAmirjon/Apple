abbedfaaf6aa75423e766bf0aeed852fd6f03fcb
= <=>
2023-12-12 16:31:55 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+let mini_part1 = document.getElementById("mini_part1")
+let mini_part2 = document.getElementById("mini_part2")
+let myImage = document.getElementById("iphone_picture");
+
+mini_part1.addEventListener("click", function() {
+    myImage.src = "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-1inch_GEO_US?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1693009279082";
+  });
+
+  mini_part2.addEventListener("click", function() {
+    myImage.src = "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-7inch_GEO_US?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1693009284541";
+  });
+
+let natural_titanium = document.getElementById("natural_titanium");
+let blue_titanium = document.getElementById("blue_titanium");
+let white_titanium = document.getElementById("white_titanium");
+let black_titanium = document.getElementById("black_titanium");
+let color = document.getElementById("color")
+
+natural_titanium.addEventListener("click", function() {
+    color.docment
+    myImage.src = "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-7inch-naturaltitanium?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1692845702708";
+  });
+
+
+blue_titanium.addEventListener("click", function() {
+    myImage.src = "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-7inch-bluetitanium?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1692845699311";
+  });
+
+white_titanium.addEventListener("click", function() {
+    myImage.src = "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-7inch-whitetitanium?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1692845706590";
+  });
+
+black_titanium.addEventListener("click", function() {
+    myImage.src = "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-7inch-blacktitanium?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1692845694698";
+  });
